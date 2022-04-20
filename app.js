90b4e4b134dc4e4c7df8962b3dc68ef4286a0bf5
GregorioM6 <87047652+GregorioM6@users.noreply.github.com>
2022-04-19 23:45:41 -0300

    desafio4

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ function ingresarRaza (r){
     if(r){
         let raza = Number(prompt("Ingresar raza:\n 1:Humano\n 2:Orco\n 3:Elfo\n 4:Enano"))
         validarRaza(raza)
-        console.log("Ha seleccionado: " + raza)
         let clasePrueba = probar();
         razaClase(raza, clasePrueba)
         
@@ -24,143 +23,116 @@ function ingresarRaza (r){
     }
 }
 
-function probar(){
-    let opcion = Number(prompt("Ingresar clase"))
-    if(opcion == 1 ){
-        return 1;
-    }else if (opcion == 2){
-        return 2;
-    }else("Opcion erronea")
-}
 
 function validarRaza (tipoRaza){
     if(tipoRaza == "1"){
-        seleccionarClaseHumano()
+        console.log("Usted selecciono: Humano")
+        console.log("--------------------------")
     }else if (tipoRaza == "2"){
-        seleccionarClaseOrco()
+        console.log("Usted selecciono: Orco")
+        console.log("--------------------------")
     }else if (tipoRaza == "3"){
-        seleccionarClaseElfo()
+        console.log("Usted selecciono: Elfo")
+        console.log("--------------------------")
     }else if (tipoRaza == "4"){
-        seleccionarClaseEnano()
+        console.log("Usted selecciono: Enano")
+        console.log("--------------------------")
     } else{
         alert("Raza incorrecta")
     }
 }
 
-
-// function seleccionarClase1(){
-//     console.log("1: Mago")
-//     console.log("2: Guerrero")
-//     console.log("3: Tirador")
-//     console.log("4: Paladin")
-//     console.log("5: Picaro")
-//     console.log("6: Nigromante")
-//     console.log("7: Druida")
-//     console.log("8: Brujo")
-//     console.log("9: Chaman")
-//     console.log("10: Cazador de demonios")
-
-function seleccionarClaseHumano(){
-    console.log("1: Mago")
-    console.log("2: Guerrero")
-    console.log("3: Tirador")
-    console.log("4: Paladin")
-    console.log("--------------------------")
-}
-
-function seleccionarClaseOrco(){
-    console.log("1: Mago")
-    console.log("2: Guerrero")
-    console.log("3: Tirador")
-    console.log("4: Paladin")
-    console.log("--------------------------")
-}
-
-function seleccionarClaseElfo(){
-    console.log("1: Mago")
-    console.log("2: Guerrero")
-    console.log("3: Tirador")
-    console.log("4: Paladin")
-    console.log("--------------------------")
-}
-
-function seleccionarClaseEnano(){
-    console.log("1: Mago")
-    console.log("2: Guerrero")
-    console.log("3: Tirador")
-    console.log("4: Paladin")
-    console.log("--------------------------")
+function probar(){
+    let opcion = Number(prompt("Ingresar clase:\n 1:Mago\n 2:Guerrero\n 3:Tirador\n 4:Paladin"))
+    if(opcion == 1 ){
+        console.log("Usted selecciono: Mago")
+        console.log("--------------------------")
+        return 1;
+    }else if (opcion == 2){
+        console.log("Usted selecciono: Guerrero")
+        console.log("--------------------------")
+        return 2;
+    }else if (opcion == 3){
+        console.log("Usted selecciono: Tirador")
+        console.log("--------------------------")
+        return 3;
+    }else if (opcion == 4){
+        console.log("Usted selecciono: Paladin")
+        console.log("--------------------------")
+        return 4;
+    }else("Es necesario ingresar un tipo de clase")
+    
 }
 
 function razaClase (tipoRaza, opcion){
     if(tipoRaza == 1 && opcion== 1 ){
-        console.log("Usted eligio Humano/Mago")
+        console.log("Usted eligio ser  Humano/Mago a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseHumanoMago()
     }else if(tipoRaza == 1 && opcion== 2 ){
-        console.log("Usted eligio Humano/Guerrero")
+        console.log("Usted eligio ser  Humano/Guerrero a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseHumanoGuerrero()
     }else if(tipoRaza == 1 && opcion== 3 ){
-        console.log("Usted eligio Humano/Tirador")
+        console.log("Usted eligio ser  Humano/Tirador a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseHumanoTirador()
     }else if(tipoRaza == 1 && opcion== 4 ){
-        console.log("Usted eligio Humano/Paladin")
+        console.log("Usted eligio ser  Humano/Paladin a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseHumanoPaladin()
     }else if(tipoRaza == 2 && opcion== 1 ){
-        console.log("Usted eligio Orco/Mago")
+        console.log("Usted eligio ser  Orco/Mago a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseOrcoMago()
     }else if(tipoRaza == 2 && opcion== 2 ){
-        console.log("Usted eligio Orco/Guerrero")
+        console.log("Usted eligio ser  Orco/Guerrero a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseOrcoGuerrero()
     }else if(tipoRaza == 2 && opcion== 3 ){
-        console.log("Usted eligio Orco/Tirador")
+        console.log("Usted eligio ser  Orco/Tirador a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseOrcoTirador()
     }else if(tipoRaza == 2 && opcion== 4 ){
-        console.log("Usted eligio Orco/Paladin")
+        console.log("Usted eligio ser  Orco/Paladin a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseOrcoPaladin()
     }else if(tipoRaza == 3 && opcion== 1 ){
-        console.log("Usted eligio Elfo/Mago")
+        console.log("Usted eligio ser  Elfo/Mago a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseElfoMago()
     }else if(tipoRaza == 3 && opcion== 2 ){
-        console.log("Usted eligio Elfo/Guerrero")
+        console.log("Usted eligio ser  Elfo/Guerrero a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseElfoGuerrero()
     }else if(tipoRaza == 3 && opcion== 3 ){
-        console.log("Usted eligio Elfo/Tirador")
+        console.log("Usted eligio ser  Elfo/Tirador a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseElfoTirador()
     }else if(tipoRaza == 3 && opcion== 4 ){
-        console.log("Usted eligio Elfo/Paladin")
+        console.log("Usted eligio ser  Elfo/Paladin a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseElfoPaladin()
     }else if(tipoRaza == 4 && opcion== 1 ){
-        console.log("Usted eligio Enano/Mago")
+        console.log("Usted eligio ser  Enano/Mago a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseEnanoMago()
     }else if(tipoRaza == 4 && opcion== 2 ){
-        console.log("Usted eligio Enano/Guerrero")
+        console.log("Usted eligio ser  Enano/Guerrero a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseEnanoGuerrero()
     }else if(tipoRaza == 4 && opcion== 3 ){
-        console.log("Usted eligio Enano/Tirador")
+        console.log("Usted eligio ser  Enano/Tirador a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseEnanoTirador()
     }else if(tipoRaza == 4 && opcion== 4 ){
-        console.log("Usted eligio Enano/Paladin")
+        console.log("Usted eligio ser  Enano/Paladin a continuacion vera sus estadisticas:")
         console.log("--------------------------")
         resultadoClaseEnanoPaladin()
     }
-    }
+}
 
-function resultadoClaseHumanoMago(){
+    function resultadoClaseHumanoMago(){
     console.log("Ataque: 60")
     console.log("Resistencia: 50")
     console.log("Mana: 100")
@@ -185,6 +157,11 @@ function resultadoClaseHumanoGuerrero(){
     console.log("Rango: 4")
 }
 
+const finalizar = () => {return "Ya se ha seleccionado su personaje"}
+
+alert(finalizar())
+
+console.log(finalizar())
 
 // // function salir(){
 // //     alert("Fin de la operacion")
